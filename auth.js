4165cb017d4c283edffcdefb4265f4a33b70d0b0
Nouran Hossameldin <111601276+nouranhossamm@users.noreply.github.com>
2023-04-20 13:34:23 +0200

    update

---
+++ auth.js
@@ -0,0 +1,51 @@
+auth.onAuthStateChanged((user) => {
+  //   console.log("user", user);
+  getTodos();
+  setupUI(user);
+});
+
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  firebase.auth().signOut();
+});
+
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+  //   console.log(email);
+  //   console.log(password);
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then(() => {
+      const modal = document.querySelector("#modal-login");
+      M.Modal.getInstance(modal).close();
+      loginForm.querySelector(".error").innerHTML = "";
+      loginForm.reset();
+    })
+    .catch((err) => {
+      loginForm.querySelector(".error").innerHTML = err.message;
+    });
+});
+
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
+  //   console.log(email);
+  //   console.log(password);
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      const modal = document.querySelector("#modal-signup");
+      M.Modal.getInstance(modal).close();
+      signupForm.querySelector(".error").innerHTML = "";
+      signupForm.reset();
+    })
+    .catch((err) => {
+      signupForm.querySelector(".error").innerHTML = err.message;
+    });
+});
